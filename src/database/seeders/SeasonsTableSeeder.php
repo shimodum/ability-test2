7fed8f93f8acc75218547a2bff6fe28af7bb4943
akihiro shimoduma <a.shimodum@gmail.com>
2024-12-17 20:29:45 +0900

    .gitignoreでstorage修正

---
+++ src/database/seeders/SeasonsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SeasonsTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('seasons')->insert([
+            ['name' => '春'],
+            ['name' => '夏'],
+            ['name' => '秋'],
+            ['name' => '冬'],
+        ]);
+    }
+}
