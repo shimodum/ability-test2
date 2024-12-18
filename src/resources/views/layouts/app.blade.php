a6b02fa8ea816148555fad1bfdbf24b5ae9cd371
akihiro shimoduma <a.shimodum@gmail.com>
2024-12-19 02:59:28 +0900

    商品詳細画面（仮）

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -6,14 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'mogitate')</title>
 
-    <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-        integrity="sha512-KfkfwPyx5fvFTe3nO9zHp3RIxQ3K/Yk3X6/vPeFOYYRwa6Y6Z6XqagkHcT/GFL4TzBgiUM5mfnBd2Gj7UoE1MQ=="
-        crossorigin="anonymous"
-        referrerpolicy="no-referrer"
-    />
-
     <!-- 共通CSS -->
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
     <link rel="stylesheet" href="{{ asset('css/common.css') }}">
